42f2f65d53985f1f1ca0d3842b27315554b861b8
alexyu <alex.yu@otus.com.tw>
2018-06-22 09:45:35 +0800

    hellow sdl

---
+++ hello_sdl/hellow_sdl.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hellow.sdl.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  06/21/2018 04:54:03 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL.h>
+#include <pthread.h>
+
+#define SCREEN_W    640
+#define SCREEN_H    480
+
+int main(int argc, char * argv[])
+{
+    SDL_Window *window = NULL;
+
+    return 0;
+}
