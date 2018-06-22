59afd2996016c4718a9e87478ca80c365e7ad55f
alexyu <alex.yu@otus.com.tw>
2018-06-22 16:51:11 +0800

    hellow

--- hello_sdl/hellow_sdl.c
+++ hello_sdl/hellow_sdl.c
@@ -26,7 +26,34 @@
 
 int main(int argc, char * argv[])
 {
-    SDL_Window *window = NULL;
+    int flag = 0;
 
+    SDL_Window *window = NULL;
+    SDL_Surface *surface = NULL;
+
+    flag = (SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+    
+    if (SDL_Init(flag) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    }
+     else {
+        window =  SDL_CreateWindow("SDL: Hellow", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_W, SCREEN_H, SDL_WINDOW_SHOWN);
+
+        if (!window) {
+            printf("Can not create window. SDL_Error: %s\n", SDL_GetError());
+        }
+        else {
+            surface = SDL_GetWindowSurface(window);
+            SDL_FillRect( surface, NULL, SDL_MapRGB( surface->format, 0x00, 0xFF, 0xFF ) );
+            SDL_UpdateWindowSurface( window );
+            SDL_Delay(2000);
+        }
+     }
+
+    if (window) {
+        SDL_DestroyWindow(window);
+    }
+    SDL_Quit();
     return 0;
 }
