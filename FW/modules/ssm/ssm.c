8fc1857579637fb03c89c37894c00c4ba0aa875a
alex.yu <alex.yu@otus.com.tw>
2020-03-03 16:10:38 +0800

    20200303.01

--- FW/modules/ssm/ssm.c
+++ FW/modules/ssm/ssm.c
@@ -1,5 +1,26 @@
 #include "ssm.h"
 
+static void ssm_init(void)    __attribute__((constructor));
+static void ssm_release(void) __attribute__((destructor));
+
+static void ssm_init(void)
+{
+	// Initialize VCOS
+	vcos_init();
+
+	// Start the shared memory support.
+	if ( vcsm_init() == -1 )
+	{
+		fprintf(stderr, "Cannot initialize vcsm device\n" );
+	}
+}
+
+static void ssm_release(void)
+{
+	// Terminate the shared memory support.
+	vcsm_exit ();
+}
+
 int ssm_test()
 {
 	unsigned int hldr;
--- FW/modules/ssm/ssm.h
+++ FW/modules/ssm/ssm.h
@@ -9,8 +9,16 @@
 #include <signal.h>
 #include <assert.h>
 
-#include "host_applications/linux/libs/sm/user-vcsm.h"
+#include "user-vcsm.h"
 #include "interface/vcos/vcos.h"
+#include "interface/mmal/mmal.h"
+#include "interface/mmal/mmal_buffer.h"
+
+typedef struct ssm_buffer_s
+{
+	int idx;
+	MMAL_BUFFER_HEADER_T bh;
+}ssm_buffer_t;
 
 int ssm_test();
 
