b0e16525b36aafba9cec38e293eca3c707402d05
alexyu7788 <alex@x200>
2020-06-22 01:34:09 +0800

    Enlargs BLK_DEV_RAM_SIZE to 737238KB

---
+++ FW/copy_patch.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+current_dir=${PWD}
+modules_folder="${current_dir}/modules"
+apps_folder="${current_dir}/apps"
+temp_folder="${current_dir}/temp"
+patch_folder="${current_dir}/patch"
+
+
+rm -rf ${temp_folder}
+mkdir -p ${temp_folder}/etc
+mkdir -p ${temp_folder}/usr/bin
+mkdir -p ${temp_folder}/usr/lib
+mkdir -p ${temp_folder}/usr/lib/plugins
+mkdir -p ${temp_folder}/usr/sbin
+
+#### Copy the libraries etc. from the modules ####
+cp -a ${modules_folder}/install_temp_modules/bin/* ${temp_folder}/usr/bin
+cp -a ${modules_folder}/install_temp_modules/lib/*.so* ${temp_folder}/usr/lib
+cp -ar ${modules_folder}/install_temp_modules/lib/plugins/*.so*  ${temp_folder}/usr/lib/plugins
+cp -ar ${modules_folder}/install_temp_modules/lib/pkgconfig ${temp_folder}/usr/lib/pkgconfig/
+
+#### Copy the bin etc. from the apps ####
+cp -a ${apps_folder}/bin/* ${temp_folder}/usr/sbin
+
+#### Copy the patch file. from the patch folder ####
+cp -a ${patch_folder}/* ${temp_folder}/
+
+#### copy default setting ####
+#if [ ! -d  ${temp_folder}/etc/default_system ];then
+#    mkdir -p ${temp_folder}/etc/default_system
+#fi
+#cp ${temp_folder}/etc/system_setting.ini ${temp_folder}/etc/default_system
+
---
+++ FW/sqfs-img.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+current_dir=${PWD}
+temp_folder="${current_dir}/temp"
+tmp_rootfs_folder="${current_dir}/tmp_rootfs"
+fake_root_session_file="${current_dir}/backup.environ"
+output_root_fs="${current_dir}/rootfs.squashfs"
+
+if [ -f ${output_root_fs} ];then
+    rm ${output_root_fs}
+fi
+
+if [ -d ${tmp_rootfs_folder} ];then
+    rm -rf ${tmp_rootfs_folder}
+fi
+
+arm-linux-strip ${temp_folder}/usr/sbin/*
+arm-linux-strip ${temp_folder}/usr/lib/*.so*
+
+fakeroot -s ${fake_root_session_file} ${current_dir}/../SDK/Rootfs/squashfs-4.4/unsquashfs -d ${tmp_rootfs_folder} ${current_dir}/../SDK/Rootfs/binary/rootfs.squashfs
+
+cp -aru ${temp_folder}/etc/* ${tmp_rootfs_folder}/etc
+cp -aru ${temp_folder}/usr/* ${tmp_rootfs_folder}/usr
+
+fakeroot -i ${fake_root_session_file} -s ${fake_root_session_file} ${current_dir}/../SDK/Rootfs/squashfs-4.4/mksquashfs ${tmp_rootfs_folder} ${output_root_fs} -comp xz -noappend -all-root -b 524288
+
+rm -rf ${temp_folder}
+rm -rf ${tmp_rootfs_folder}
+rm ${fake_root_session_file}
